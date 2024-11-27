fc1c331dc5f57dd9406b6758abeec3c7a9785e92
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-11-27 15:40:17 +0530

    use State funtion(react hook)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.css'
 import ChildButton from "./components/Button.tsx";
+import {useState} from "react";
+
 function App() {
 
+    // let content :string = 'Dashboard';
+    const[content, setContent] = useState('');
     function handleClick(type:string){
         console.log(type)
         alert(type)
+        // content=type
+        setContent(type)
     }
   return (
     <div>
         <ChildButton name={'Dashboard'} onClick={handleClick}/>
         <ChildButton name={'Add Customer'} onClick={handleClick}/>
         <ChildButton name={'Delete Customer'} onClick={handleClick}/>
+        <br/>
+        {content}
     </div>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export default function ChildButton(props) {
     const name = props.name
     return(
-        <button className="button" onClick={() => props.onClick(name)}>{props.name}</button>
+        <button onClick={() => props.onClick(name)}>{props.name}</button>
     )
 }
