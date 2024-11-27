98799a704801e9bd2b4895a94e513872d0247c8b
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-11-27 16:21:31 +0530

    use State (singale line incriment)

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ function App() {
 
 
     const[count, setContent] = useState(0);
-    function increeseCount(type){
-        setContent(count+1)
-    }
+    // function increeseCount(type){
+    //     setContent(count+1)
+    // }
   return (
     <div>
-        <ChildButton name ={"Click : "+count} count={count} onClick={increeseCount}/>
-        <br/>
-        {/*{count}*/}
+        {/*<ChildButton name ={"Click : "+count} count={count} onClick={count+1}/>*/}
+        {/*<button name ={"Click : "+count} onClick={setContent(count+1)}/></button>*/}
+      <button onClick={()=>{setContent((count+1))}} >Click : {count}</button>
     </div>
   )
 }
